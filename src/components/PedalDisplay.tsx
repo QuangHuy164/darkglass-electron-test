ed841a67cdc4fdf6e5f20ebc02146adc9f0f323e
Huy Nguyen <nghuy164@gmail.com>
2026-03-28 05:06:17 +0200

    update

--- src/components/PedalDisplay.tsx
+++ src/components/PedalDisplay.tsx
@@ -1,12 +1,41 @@
 /* eslint-disable prettier/prettier */
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+
+// import KnobImg from '../../assets/Alpha-Omicron-assets/Knob-189x189.webp'
+import BtnOffImg from '../../assets/Alpha-Omicron-assets/Push-Button-Off-134x134.webp';
+import BtnOnImg from '../../assets/Alpha-Omicron-assets/Push-Button-On-134x134.webp';
+import LedOffImg from '../../assets/Alpha-Omicron-assets/LED-Off-119x119.webp';
+import LedOnImg from '../../assets/Alpha-Omicron-assets/LED-On-119x119.webp';
+import ActOffImg from '../../assets/Alpha-Omicron-assets/Actuator-Off-166x166.webp';
+import ActOnImg from '../../assets/Alpha-Omicron-assets/Actuator-On-166x166.webp';
+import ChasisImg from '../../assets/Alpha-Omicron-assets/Chasis-1000x1000.webp';
+//cannot resolve module ../../assets
 
 function PedalDisplay() {
-    const navigate = useNavigate()
-    return <div>
-    <button type="button" onClick={() => navigate('/')}>Return to homepage</button>
-        
+  const navigate = useNavigate();
+  const [isMainOn, setIsMainOn] = useState(false);
+
+  return (
+    <div
+      id="pedal-container"
+      style={{ width: '748px', position: 'relative', margin: 'auto' }}
+    >
+      <img
+        src={isMainOn ? LedOnImg : LedOffImg}
+        className="component led led-light"
+        alt={isMainOn ? 'Pedal Active' : 'Pedal Inactive'}
+      />
+      <img
+        src={isMainOn ? ActOnImg : ActOffImg}
+        className="actuator-image"
+        alt={isMainOn ? 'Bypass Engaged' : 'Bypass Disengaged'}
+      />
+      <button type="button" onClick={() => navigate('/')}>
+        Return to homepage
+      </button>
     </div>
+  );
 }
 
-export default PedalDisplay
+export default PedalDisplay;
